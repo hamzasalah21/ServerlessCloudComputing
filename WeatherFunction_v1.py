972d62b21a48a8d8cf11637b5578c9c5c9e907d3
hamzasalah21 <hamza.salah211@gmail.com>
2020-04-21 16:36:56 -0400

    k

--- WeatherFunction_v1.py
+++ WeatherFunction_v1.py
@@ -12,13 +12,18 @@ def background_calculation():
     result = "The weather is sunny in Montreal."
 
 def main():
-    thread = threading.Thread(target=background_calculation)
-    thread.start()
+    counter = 0
+    while True:
+        thread = threading.Thread(target=background_calculation)
+        thread.start()
 
-    # TODO: wait here for the result to be available before continuing!
+        # TODO: wait here for the result to be available before continuing!
 
-    thread.join()
-    print('The result is', result)
+        thread.join()
+        print('The result is', result)
+        counter += 1
+        if counter == 20:
+            break
 
 if __name__ == '__main__':
     main()
